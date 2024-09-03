4535086a7a3973e894c64a6e175ead9808d42ccf
spidermunking <105009002+spidermunking@users.noreply.github.com>
2024-09-03 10:51:36 -0600

    Update Calc 2.cpp

--- Calc 2.cpp
+++ Calc 2.cpp
@@ -3,11 +3,13 @@
 #include <string>
 
 using namespace std;
-void history();
 
 double num = 0;
 double num2 = 0;
-float ans = 0;
+double ans = 0;
+int number = 0;
+int number2 = 0;
+int answer = 0;
 char op = 0;
 int loop = 1;
 string h = "";
@@ -19,38 +21,46 @@ int main(void){
         cin >> op;
         cin >> num2;
         switch(op){
-        case '+':
-            ans = num + num2;
-        break;
-        case '-':
-            ans = num - num2;
-        break;
-        case '*':
-            ans = num * num2;
-        break;
-        case '/':
-            ans = num / num2;
-        break;
-        case '^':
-            ans = pow(num,num2);
-        break;
-        case '<':
-            ans = pow(num, (1 / num2));
-        break;
-        case '=':
-            loop = 0;
-            cout << "Yur dun!" << endl;
-        break;
-        case 'h': case 'H':
-            cout << h << endl;
-            op = ' ' ;
-        break;
+            case '+':
+                ans = num + num2;
+            break;
+            case '-':
+                ans = num - num2;
+            break;
+            case '*':
+                ans = num * num2;
+            break;
+            case '/':
+                ans = num / num2;
+            break;
+            case '^':
+                ans = pow(num,num2);
+            break;
+            case '<':
+                ans = pow(num, (1 / num2));
+            break;
+            case '=':
+                loop = 0;
+                cout << "Yur dun!" << endl;
+            break;
+            case 'h': case 'H':
+                cout << h << endl;
+                op = 0;
+            break;
         }
         cout << num << op << num2 << "=" << ans << endl;
-        h = h + to_string(num)+ op + to_string(num2)+ "=" + to_string(ans)+ "\n";
-        
+
+        //this next part changes 
+        //some variables to integers 
+        //for the sake of ease. idk
+        number = num;
+        number2 = num2;
+        answer = ans;
+
+        h = h + to_string(number)+ op + to_string(number2)+ "=" + to_string(answer)+ "\n";
     }
 
     return 0;
 }
+
 
